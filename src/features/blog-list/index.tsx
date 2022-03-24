57e89eb25f60db048e971bf0fe79906b72734896
dattq21 <90371851+dattq21@users.noreply.github.com>
2022-03-24 15:21:47 +0700

    feat(): set up libs and route

---
+++ src/features/blog-list/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function BlogList() {
+    console.log('BLOG LIST');
+    return <div className='h1'>Blog list</div>;
+}
+
+export default BlogList;
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <React.StrictMode>
