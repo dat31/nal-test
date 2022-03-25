a7cbe6aeb03099c43918055213c1884b6ada8c83
dattq21 <90371851+dattq21@users.noreply.github.com>
2022-03-25 20:42:20 +0700

    feat(package): add & setup packages

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,8 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.render(
     <React.StrictMode>
