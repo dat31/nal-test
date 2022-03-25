1022162803d3cfadfd75922ad8f6cad82f220f18
dattq21 <90371851+dattq21@users.noreply.github.com>
2022-03-25 21:12:19 +0700

    feat(): lazy load BlogDetail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import BlogList from './features/blog-list';
-import BlogDetail from './features/blog-detail';
 import { Header } from './components';
 import { Slide, ToastContainer } from 'react-toastify';
+
+const BlogDetail = React.lazy(() => import('./features/blog-detail'));
 
 function App() {
     return (
@@ -13,12 +14,14 @@ function App() {
                 <Header />
                 <div className='container'>
                     <ToastContainer transition={Slide} position={'bottom-center'} />
-                    <Routes>
-                        <Route path='/' element={<Navigate to='/blogs' />} />
-                        <Route element={<BlogList />} path='/blogs' />
-                        <Route element={<BlogDetail isEdit />} path='blogs/:id' />
-                        <Route element={<BlogDetail />} path='blogs/create' />
-                    </Routes>
+                    <Suspense fallback={<span className='spinner-border spinner-border-sm mr-2' role='status' aria-hidden='true'></span>}>
+                        <Routes>
+                            <Route path='/' element={<Navigate to='/blogs' />} />
+                            <Route element={<BlogList />} path='/blogs' />
+                            <Route element={<BlogDetail isEdit />} path='blogs/:id' />
+                            <Route element={<BlogDetail />} path='blogs/create' />
+                        </Routes>
+                    </Suspense>
                 </div>
             </Router>
         </>
